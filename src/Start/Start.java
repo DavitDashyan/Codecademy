97371136ad483489b07d758256ff3014b25ce2d8
DavitDashyan <davit.dashyan@icloud.com>
2023-04-14 11:35:27 +0200

    Initial commit
    Added new repo, other one didn't work

---
+++ src/Start/Start.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Start;
+
+import Login.LoginPage;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Start extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Welcome to Codecademy");
+
+        // Create welcomeTitle
+        Text welcomeTitle = new Text("Welcome");
+        welcomeTitle.setFont(new Font(46));
+
+        // Create startButton
+        Button startButton = new Button("Start");
+        startButton.setOnAction(event -> {
+            // Maak een instantie van LoginPage
+            LoginPage loginPage = new LoginPage();
+
+            try {
+                // Open de login pagina
+                loginPage.start(new Stage());
+
+                // Sluit de huidige pagina
+                primaryStage.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        // Create vBoxWelcome
+        VBox vBoxWelcome = new VBox(10);
+        vBoxWelcome.setAlignment(Pos.CENTER);
+        vBoxWelcome.getChildren().addAll(welcomeTitle, startButton);
+
+        // Create welcomeAnchor
+        AnchorPane welcomeAnchor = new AnchorPane(vBoxWelcome);
+        AnchorPane.setTopAnchor(vBoxWelcome, 0.0);
+        AnchorPane.setBottomAnchor(vBoxWelcome, 0.0);
+        AnchorPane.setLeftAnchor(vBoxWelcome, 0.0);
+        AnchorPane.setRightAnchor(vBoxWelcome, 0.0);
+
+        // Set the scene
+        Scene scene = new Scene(welcomeAnchor, 400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
+
---
+++ src/codecademy/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codecadamy;
+
+import Start.Start;
+import javafx.application.Application;
+
+/**
+ *
+ * @author dashy
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Application.launch(Start.class);
+    }
+
+}
