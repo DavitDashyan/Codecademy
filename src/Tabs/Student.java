f7ff8b45981abb4d663fa040716c86ab99dfce07
Jelmer-Ketelaar <jelmer@ketelaar.me>
2023-04-15 11:49:20 +0200

    Student.java class toegevoegd

--- src/Tabs/Student.java
+++ src/Tabs/Student.java
@@ -1,19 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Tabs;
 
-/**
- *
- * @author jelmer
- */
-class Student {
+import java.time.LocalDate;
 
-    private String name;
-    private String email;
-    private Integer address_id;
-    private String birth_date;
+public class Student {
 
+    private static int lastUsedId = 0;
+
+    private final int id;
+    private final String name;
+    private final String email;
+    private final LocalDate birthDate;
+    private final String gender;
+    private final String city;
+    private final String country;
+
+    public Student(String email, String name, LocalDate birthDate, String gender, String city, String country) {
+        lastUsedId++;
+        this.id = lastUsedId;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.city = city;
+        this.country = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
 }
