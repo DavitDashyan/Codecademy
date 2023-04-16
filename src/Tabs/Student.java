7ec3a6f8af465672d85553677ca3c77a750e4731
Jelmer-Ketelaar <jelmer@ketelaar.me>
2023-04-16 17:39:39 +0200

    Update Student.java

--- src/Tabs/Student.java
+++ src/Tabs/Student.java
@@ -6,13 +6,13 @@ public class Student {
 
     private static int lastUsedId = 0;
 
-    private final int id;
-    private final String name;
-    private final String email;
-    private final LocalDate birthDate;
-    private final String gender;
-    private final String city;
-    private final String country;
+    private int id;
+    private String name;
+    private String email;
+    private LocalDate birthDate;
+    private String gender;
+    private String city;
+    private String country;
 
     public Student(String email, String name, LocalDate birthDate, String gender, String city, String country) {
         lastUsedId++;
@@ -51,5 +51,54 @@ public class Student {
 
     public String getCountry() {
         return country;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @param birthDate the birthDate to set
+     */
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
