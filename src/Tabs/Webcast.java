ff106fa87001c37a4b81cded376453946518e3a9
Jelmer-Ketelaar <jelmer@ketelaar.me>
2023-04-16 21:58:16 +0200

    Fixed code and added CRUD for webcast

---
+++ src/Tabs/Webcast.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tabs;
+
+/**
+ *
+ * @author jelmer
+ */
+public class Webcast {
+
+    private String title;
+    private String description;
+    private String speakerName;
+    private String organization;
+    private int duration;
+    private String url;
+
+    public Webcast(String title, String description, String speakerName, String organization, int duration, String url) {
+        this.title = title;
+        this.description = description;
+        this.speakerName = speakerName;
+        this.organization = organization;
+        this.duration = duration;
+        this.url = url;
+    }
+
+    // Getters and setters for each field
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSpeakerName() {
+        return speakerName;
+    }
+
+    public void setSpeakerName(String speakerName) {
+        this.speakerName = speakerName;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
